5295277713bd59eeb33fb1d6f2ccf87a23402192
khmoryz <kokkenro0520@gmail.com>
2026-01-18 11:24:18 +0900

    chore: バージョンを0.1.7に更新
    - レスポンシブデザインの改善
    - 辞書パネルの表示閾値を1000pxに変更
    - プロジェクトヘッダーを中央寄せに変更

--- Casks/mekakushi.rb
+++ Casks/mekakushi.rb
@@ -1,8 +1,8 @@
 cask "mekakushi" do
-  version "0.1.6"
-  sha256 "fd730fa8cc492779ce72d27d1034f10413c191c6795ba325480b9644f4dee41b"
+  version "0.1.7"
+  sha256 "f4fdb5b6236efbb58ae76231b965b299b7edb500707de8d3f45659a316f84adf"
 
-  url "https://github.com/khmoryz/mekakushi/releases/download/v#{version}/Mekakushi-0.1.0-arm64-mac.zip"
+  url "https://github.com/khmoryz/mekakushi/releases/download/v#{version}/Mekakushi-#{version}-arm64-mac.zip"
   name "Mekakushi"
   desc "Screen recording tool with privacy protection for sensitive information"
   homepage "https://github.com/khmoryz/mekakushi"
